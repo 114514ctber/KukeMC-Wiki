3a5f3be2538d9cbc59df6decf24d42bbe8b81f02
0ctber <143686153+114514ctber@users.noreply.github.com>
2025-01-24 19:26:00 +0800

    Update Contributor.vue

--- docs/.vuepress/theme/components/Contributor.vue
+++ docs/.vuepress/theme/components/Contributor.vue
@@ -18,11 +18,7 @@ const props = defineProps<PlumeHomeConfigBase & {
       <h1 style="text-align:center;font-size:28px;font-weight:bolder">文档贡献者名单</h1>
     </div>
     <CardGrid :cols="{ sm: 1, md: 3, lg: 3 }">
-      <Card
-        title="0ctber"
-        icon="https://crafthead.net/avatar/0ctber"
-        style="text-align: center;"
-      >
+      <Card>
         <template #icon>
           <img
             src="https://crafthead.net/avatar/0ctber"
@@ -31,10 +27,10 @@ const props = defineProps<PlumeHomeConfigBase & {
           />
         </template>
         <template #title>
-          <h2 style="font-size: 20px; font-weight: bold;">0ctber</h2>
+          <h2 style="font-size: 20px; font-weight: bold; margin: 10px 0;">0ctber</h2>
         </template>
         <template #default>
-          <p style="font-size: 14px; color: #666;">服务器管理员</p>
+          <p style="font-size: 14px; color: #666; margin: 5px 0;">服务器管理员</p>
         </template>
       </Card>
     </CardGrid>
