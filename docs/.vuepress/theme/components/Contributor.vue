9a5a8789b91e660541cffdb4a95d0f90b4ceba28
0ctber <143686153+114514ctber@users.noreply.github.com>
2025-01-25 03:09:37 +0800

    Update Contributor.vue

--- docs/.vuepress/theme/components/Contributor.vue
+++ docs/.vuepress/theme/components/Contributor.vue
@@ -29,7 +29,7 @@ const props = defineProps<PlumeHomeConfigBase & {
             <div>
               <h2 style="font-size: 24px; font-weight: bold; margin: 0;">0ctber</h2>
               <p style="font-size: 14px; color: #666; margin: 5px 0;">服务器管理员/责任编辑</p>
-              <p style="font-size: 14px; color: #666; margin: 5px 0;">文档站、监控站、官网运维，一个普通的志愿者</p>
+              <p style="font-size: 14px; color: #666; margin: 5px 0;">一个普通的志愿者</p>
             </div>
           </div>
         </template>
@@ -44,8 +44,24 @@ const props = defineProps<PlumeHomeConfigBase & {
             />
             <div>
               <h2 style="font-size: 24px; font-weight: bold; margin: 0;">kukemc</h2>
-              <p style="font-size: 14px; color: #666; margin: 5px 0;">服主/吉祥物</p>
+              <p style="font-size: 14px; color: #666; margin: 5px 0;">服主/责任编辑</p>
               <p style="font-size: 14px; color: #666; margin: 5px 0;">古希腊掌管最高神犬的生物</p>
+            </div>
+          </div>
+        </template>
+      </Card>
+      <Card>
+        <template #default>
+          <div style="display: flex; align-items: center; gap: 16px;">
+            <img
+              src="https://m.ccw.site/gandi_application/user_assets/6b7325e5925229a458d6b6abd107401d.jpg"
+              alt="zurnm"
+              style="width: 80px; height: 80px; border-radius: 5%; align-self: flex-end;"
+            />
+            <div>
+              <h2 style="font-size: 24px; font-weight: bold; margin: 0;">zurnm</h2>
+              <p style="font-size: 14px; color: #666; margin: 5px 0;">编辑助理</p>
+              <p style="font-size: 14px; color: #666; margin: 5px 0;">负责站点日常维护</p>
             </div>
           </div>
         </template>
