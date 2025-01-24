86eeaead8ff6b8bc06e1f7036a50b9b1c29a48ad
0ctber <143686153+114514ctber@users.noreply.github.com>
2025-01-24 15:13:18 +0800

    Update navbar.ts

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -2,14 +2,14 @@ import { defineNavbarConfig } from 'vuepress-theme-plume'
 
 export const navbar = defineNavbarConfig([
   { text: '首页', icon: 'icon-park-outline:home' , link: '/'  },
-  { text: '官网', link: 'https://kuke.ink' },
   {
     text: '更多',
     icon: 'icon-park-outline:more-two',
     items: [
-      { text: '在玩玩家列表', link: 'https://kuke.ink/player.html' },
-      { text: '运行状态监控', link: 'https://status.kuke.ink' },
-      { text: '玩家封禁名单', link: 'https://kuke.ink/banlist.html' },
+      { text: '官网', icon: 'icon-park-outline:home' , link: 'https://kuke.ink' },
+      { text: '在线玩家列表', icon: 'material-symbols:patient-list' , link: 'https://kuke.ink/player.html' },
+      { text: '运行状态监控', icon: 'material-symbols:monitor-heart-outline' , link: 'https://status.kuke.ink' },
+      { text: '玩家封禁名单', icon: 'material-symbols:patient-list' , link: 'https://kuke.ink/banlist.html' },
     ]
   },
 ])
