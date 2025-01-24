a0ded60d5ecd01d27a35ed19b8da8adb699ac215
0ctber <143686153+114514ctber@users.noreply.github.com>
2025-01-24 14:14:00 +0800

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -148,7 +148,6 @@ export default defineUserConfig({
         repoId: 'R_kgDONufNJA',
         category: '评论空间~',
         categoryId: 'DIC_kwDONufNJM4CmS8Z',
-        reactions-enabled="0"
      },
     },
 
