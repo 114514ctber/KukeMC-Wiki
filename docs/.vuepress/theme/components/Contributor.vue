b2dbaa3b4887d680a70c58d94e6c2065a4cfdc2f
0ctber <143686153+114514ctber@users.noreply.github.com>
2025-01-24 18:35:27 +0800

    Create Contributor.vue

---
+++ docs/.vuepress/theme/components/Contributor.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import type { PlumeHomeConfigBase } from 'vuepress-theme-plume'
+
+const props = defineProps<PlumeHomeConfigBase & {
+  // 组件 props, frontmatter 中的属性将会传递给组件
+}>()
+</script>
+
+<template>
+  <VPHomeBox
+    :type="type"
+    class="title"
+    :background-image="backgroundImage"
+    :background-attachment="backgroundAttachment"
+    :full="false"
+  >
+
+  <div><h1 style="text-align:center;font-size:28px;font-weight:bolder">文档贡献者名单</h1></div>
+    <CardGrid :cols="{ sm: 1, md: 3, lg: 3 }">
+        <Card title="0ctber" icon="https://crafthead.net/avatar/0ctber"></Card>
+    </CardGrid>
+  </VPHomeBox>
+</template>
