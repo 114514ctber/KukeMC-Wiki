bc67d06cb5b894ca4b20762257777249dc8e94ec
0ctber <143686153+114514ctber@users.noreply.github.com>
2025-01-26 11:49:21 +0800

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -54,7 +54,7 @@ export default defineUserConfig({
 
     plugins: {
        MeiliSearchPlugin({
-        host: 'https://search.0ctber.cn',
+        host: '154.37.214.183:7730',
         apiKey: 'K3mP7F8aQ1xR5bZ9',
         indexUid: 'KukeWiki',
        })
